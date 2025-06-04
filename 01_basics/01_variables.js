a0701322e175aaf6f1199614ffe2a171417c66a9
Rohannz01 <136063672+Rohannz01@users.noreply.github.com>
2025-06-04 13:07:31 +0000

    variables & datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 14553
+let accountEmail ="rohan@gmail.com"
+var accountPassword ="12345"
+accountCity ="Nagpur"
+let accountState;
+
+//const accountId ="6"// not allowed
+
+accountEmail ="hc@ad.com"
+accountPassword="5645"
+accountCity="pune"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/* prefer not to use var
+because of issue in block space and functional scope
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict";// treat all JS  code as never version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3) // code readability should be high
+
+console.log("Rohan")
+
+let name="rohan"
+let age =23
+let isLoggedIn = false
+
+
+// these are some primitive  data types
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof "rohan");
+
+
+
