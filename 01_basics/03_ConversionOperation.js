b13324c2d897552abefb849ad717a7a264c3e8df
Rohannz01 <136063672+Rohannz01@users.noreply.github.com>
2025-06-05 11:16:46 +0000

    more operators

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -21,8 +21,50 @@ let booleanIsLoggeddIn = Boolean(isLoggedIn)
 let someNumber = 07
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ********************Operations*****************
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue);
+
+let str1 ="Hello"
+let  str2 =" Rohan"
+
+let str3 = str1 + str2
+//console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(true);
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+/*If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.
+
+If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
