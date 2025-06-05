a6119449bcfce5dc97e3fc40fde75286592fcc68
Rohannz01 <136063672+Rohannz01@users.noreply.github.com>
2025-06-05 11:53:13 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,39 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id =  Symbol('123')
+const anotherId = Symbol('123')
+
+//console.log(id === anotherId);
+
+
+const bigNumber = 484548
+
+
+
+
+//  Refrence (Non Primitive)
+
+// Array, Objects, Functions
+
+const heros =["batman", "superman"]
+
+//objects are defined in {}
+{
+    name:"rohan",
+    age=23
+}
+
+const myfunction = function(){
+    console.log("Helllo  world");
+
+}
+
