ae734df76d9f7cae6e4434a8e758562698e261f4
Rohannz01 <136063672+Rohannz01@users.noreply.github.com>
2025-06-06 17:15:41 +0000

    math & num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,46 @@
+const score = 500
+//console.log(score);
+
+
+const balance = new Number(200)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+
+const otherNumber = 22.968
+//console.log(otherNumber.toPrecision(2));
+// to.Precision focuses on the digits before the decimal
+
+const hundreds = 100000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+//++++++++++++++++++++++++++ Maths ++++++++++++++++
+
+console.log(Math);
+
+// console.log(Math.abs(-4)); //Converts negative to positve
+// console.log(Math.round(4.3)); // Rounds of the given number
+// console.log(Math.ceil(4.3)); // rounds  of the number  to the higher  digit
+// console.log(Math.floor(5.2)); // rounds of the number to the lower digit
+// console.log(Math.min(4, 5, 6, 7)); // gives the min value in given number set
+// console.log(Math.max(5, 6, 7, 8)); // gives the max value in given number  set
+
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log((Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+ 
+
+
+
+
+
+
+
+
+
