381093da436ace65caefa20d36a9f668e373c76f
Rohannz01 <136063672+Rohannz01@users.noreply.github.com>
2025-06-06 12:02:16 +0000

    types of strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,55 @@
+const name = "Rohan"
+const totalGoals = 50
+
+
+//console.log(name + totalGoals);
+// this is a bad practice  looks disorganised
+
+//console.log(`Hello my name is ${name} and my totalGoals  are ${totalGoals} `);
+
+const gameName =  new String('Rohan-sm')
+
+// console.log(gameName[0] );
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('h'));
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// const  anotherString = gameName.slice(-6, 3)
+// console.log(anotherString);
+
+const newStringOne = "   Rohan  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://rohan.com/rohan%20mankar"
+console.log(url.replace('%20', '-'));
+
+console.log(gameName.split('-'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
