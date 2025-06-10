44e13a9f2cc87a551c5e956163c610d7f3d95edd
Rohannz01 <136063672+Rohannz01@users.noreply.github.com>
2025-06-10 07:37:06 +0000

    json

--- 02_basics/04_objets.js
+++ 02_basics/04_objets.js
@@ -46,10 +46,37 @@ const users = [
     
 ]
 users[1].email
-console.log(tinderUser);
+//console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+//console.log(Object.keys(tinderUser));
+//console.log(Object.values(tinderUser));
+//console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+//console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+const course = {
+    coursename: "js in english",
+    price: "999",
+    courseInstructor: "Rohan"
+
+}
+
+// course.courseInstructor
+
+const {courseInstructor} = course
+
+console.log(courseInstructor);
+
+// {
+//     "name": "Rohan",
+//     "coursename": "js in hindi",
+//     "price: "free 
+
+// }
+
+[
+    {},
+    {},
+    {}
+]
