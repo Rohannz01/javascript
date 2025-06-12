ac33081c12117b2371bda24240882d9816f138b8
Rohannz01 <136063672+Rohannz01@users.noreply.github.com>
2025-06-12 08:17:17 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -7,14 +7,53 @@ let a =300
 if (true) {
     let a = 10
     const b  = 20
-    console.log("INNER:",a );
+    // console.log("INNER:",a );
     
     
 }
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
 
+function one (){
+    const username = "Rohan"
 
+    function two(){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    two()
+}
+
+//one()
+
+
+if (true) {
+    const username = "Rohan"
+    if (username === "Rohan"){
+        const website =" youtube"
+        // console.log(username + website);
+
+        
+    }
+}
+
+// ++++++++++++++++Intresting +++++++++++++
+
+function addone(num){
+    return num  + 1
+
+}
+
+addone(5) // this is one way of writing function in javascript
+
+
+const addTwo = function(num){
+    return num + 2
+
+}
+
+addTwo(5) // this is also a type of writing a function
