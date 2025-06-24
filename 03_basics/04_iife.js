cfe23e3e5330aea38824ae6d5486b4155f3a0147
Rohannz01 <136063672+Rohannz01@users.noreply.github.com>
2025-06-24 06:07:04 +0000

    iffe

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,11 @@
+// Immediately Invoked Function Expressions (IFFE)
+
+(function chai (){
+    console.log(`DB CONNECTED`);
+    
+})();
+
+( function aurcode() {
+    console.log(`DB CONNECTED TWO`);
+    
+})()
